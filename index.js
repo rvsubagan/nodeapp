5885e4fc674f32fae2b7a64313c915eb3a42c956
inamicode <inamicode@gmail.com>
2022-11-22 07:49:17 +0800

    second commit

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import ticketsRoutes from './routes/tickets.js';
+'use strict';
 
+const express = require('express');
 
+// Constants
+const PORT = 8082;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-dotenv.config();
-
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/tickets', ticketsRoutes);
-
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Welcome to Node API! "});
+  res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.DB_URL)
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`)))
-.catch((error) => console.log(error.message));
-
-
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
