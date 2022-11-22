04178cf6dca0f876d95ad3781bec14e0c88c1f76
inamicode <inamicode@gmail.com>
2022-11-22 11:12:28 +0800

    node app 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Matrix REloaded!');
+  res.send('Node App 1!');
 });
 
 app.listen(PORT, HOST, () => {
