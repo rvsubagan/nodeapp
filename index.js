ac79f9d4c2e9bba60bf93eefe369dc95ce043fd5
inamicode <inamicode@gmail.com>
2022-11-22 15:43:45 +0800

    Actionsgit add .!

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Node App 1!');
+  res.send('Github Actions!');
 });
 
 app.listen(PORT, HOST, () => {
